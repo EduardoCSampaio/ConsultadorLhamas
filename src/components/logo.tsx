bbccb9a755a59a0415678f7d0b4627d568d4c624
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-10-08 19:18:00 +0000

    Pode mudar a imagem, deixa a escrita mesmo a imagem ta vindo meio bugada

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,9 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-inherit", className)}>
-      <div className="bg-primary/10 dark:bg-primary/20 p-2 rounded-lg flex items-center justify-center">
-        <Image src="/favicon.ico" alt="Lhamascred Logo" width={20} height={20} className="h-5 w-5" />
-      </div>
       <span className="text-lg font-bold tracking-tight font-headline">Lhamascred</span>
     </Link>
   );
