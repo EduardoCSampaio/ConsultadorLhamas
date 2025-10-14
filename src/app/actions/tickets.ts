4c2adfc1895aa13a8c4b0889ce5c6dcd2579f51a
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-10-14 17:06:33 +0000

    Erro ao Carregar Chamados r.exists is not a function

--- src/app/actions/tickets.ts
+++ src/app/actions/tickets.ts
@@ -106,7 +106,7 @@ async function getNextTicketNumber(): Promise<string> {
     let newNumber = 1;
     await firestore.runTransaction(async (transaction) => {
         const counterDoc = await transaction.get(counterRef);
-        if (!counterDoc.exists) {
+        if (!counterDoc.exists()) {
             transaction.set(counterRef, { currentNumber: newNumber });
         } else {
             newNumber = (counterDoc.data()?.currentNumber || 0) + 1;
