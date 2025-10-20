3f187360d443caa56f76164763d0f966085b1903
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-10-20 20:49:04 +0000

    que daora, mudou nada, tá tudo errado ainda...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)'],
-        headline: ['var(--font-inter)'],
+        headline: ['var(--font-alegreya)'],
       },
       colors: {
         background: 'hsl(var(--background))',
