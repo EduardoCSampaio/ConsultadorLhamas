90fda96831291de3d9c4ec01b0792e8b500b3a18
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-10-31 18:50:07 +0000

    Quer dar um tapa nesse visual? Deixar ele mais bonitinho talvez? do sist

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,12 @@
-
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import Image from "next/image";
+import { Landmark } from "lucide-react";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-inherit", className)}>
-      <div className="bg-primary/10 dark:bg-primary/20 p-2 rounded-lg flex items-center justify-center">
-        <Image src="/favicon.ico" alt="Lhamascred Logo" width={20} height={20} className="h-5 w-5" />
+      <div className="bg-primary/20 dark:bg-primary/20 p-2 rounded-lg flex items-center justify-center">
+        <Landmark className="h-5 w-5 text-primary" />
       </div>
       <span className="text-lg font-bold tracking-tight font-headline">Lhamascred</span>
     </Link>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -30,10 +30,10 @@ const CardHeader = React.forwardRef<
 CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef<
-  HTMLDivElement,
+  HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <div
+  <h3
     ref={ref}
     className={cn(
       "font-headline text-2xl font-semibold leading-none tracking-tight",
@@ -41,7 +41,7 @@ const CardTitle = React.forwardRef<
     )}
     {...props}
   />
-));
+))
 CardTitle.displayName = "CardTitle"
 
 
@@ -49,12 +49,12 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <div
+  <p
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-));
+))
 CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef<
@@ -78,5 +78,3 @@ const CardFooter = React.forwardRef<
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
-
-    
