8e952a5168be1afbfe736288f2aee974f401b0ea
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-10-31 18:54:14 +0000

    Gostei, mas ainda quero usar o dourado e a logo quero que seja a imagem

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,18 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { Gem } from "lucide-react";
+import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-3 text-inherit", className)}>
-      <div className="bg-primary/20 p-2 rounded-lg flex items-center justify-center">
-        <Gem className="h-5 w-5 text-primary" />
+      <div className="bg-primary/20 p-1 rounded-lg flex items-center justify-center">
+        <Image 
+            src="/lhamas.jpeg" 
+            alt="Lhamascred Logo" 
+            width={32} 
+            height={32} 
+            className="rounded-md"
+        />
       </div>
       <span className="text-xl font-bold tracking-tighter font-headline">Lhamascred</span>
     </Link>
